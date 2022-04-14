d5ecbb9eb1f789f75f53228a7a9dd9cca685f24f
IanJCV <ausp.jcv@gmail.com>
2022-04-14 22:06:29 +0200

    Multiple changes (LainGame.cpp, CMakeLists, gitignore)
    Added a notice to the main cpp file, cleaned up the CMakeLists file, and added the out folder to the gitignore.

--- LainGame.cpp
+++ LainGame.cpp
@@ -1,8 +1,19 @@
-﻿// LainGame.cpp : Defines the entry point for the application.
-//
+﻿/* Copyright 2021-2022 by Ian JCV, 'Emil' Williams
+ *
+ * LainGame is libre software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, under version 3 of the License.
+ *
+ * LainGame is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with LainGame. If not, see <https://www.gnu.org/licenses/>.
+ */
 
 #include "LainGame.h"
-#include "file_systems/file_packer.h"
 
 using namespace std;
 
