bb731ecddd6eefbf3cbc870f7987a20f9a2a3e40
IanJCV <ausp.jcv@gmail.com>
2022-04-14 20:42:20 +0200

    Adding stuff

---
+++ LainGame.cpp
@@ -0,0 +1,13 @@
+﻿// LainGame.cpp : Defines the entry point for the application.
+//
+
+#include "LainGame.h"
+#include "file_systems/file_packer.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
